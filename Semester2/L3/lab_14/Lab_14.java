46ea3fd4953097a52041f81f529faeff979bfa97
Olexii <75754524+OlexiiT@users.noreply.github.com>
2021-05-22 13:54:08 +0300

    Create Lab_14.java

---
+++ Semester2/L3/lab_14/Lab_14.java
@@ -0,0 +1,13 @@
+package lab_14;
+
+public class Lab_14 {
+
+    public static void main(String[] args) {
+        
+        Task3.main(args);
+        System.out.println("\n\n");
+        Task7.main(args);
+    
+    }
+        
+}
